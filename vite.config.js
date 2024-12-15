429f373996d8d1d3106cf033e4c2c3b399ee81d4
Mohamed omara <66154169+omara2001@users.noreply.github.com>
2024-12-15 22:32:11 +0200

    Initial commit for llminaboxClient

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        client: "index.html",
+        assistant: "src/assistant/index.tsx"
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === "client") return "client.js";
+          if (chunkInfo.name === "assistant") return "assistant.js";
+        },
+        assetFileNames: () => {
+          return "assets/[name].[ext]";
+        }
+      }
+    }
+  }
+});
